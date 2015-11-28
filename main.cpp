2b96025295f31e9ed06617b73a033edea33a0ca9
Peter Gal <galpeter@inf.u-szeged.hu>
2015-11-28 15:05:11 +0100

    Revert to both GPU and CPU calculations

--- main.cpp
+++ main.cpp
@@ -19,29 +19,22 @@ static Measurement measureMultiply(Matrix& lhs, Matrix& rhs)
     Operations* gpu = new GpuOperations();
 
     steady_clock::time_point gpuStart = std::chrono::steady_clock::now();
-    try {
-        Matrix gpuMatrix = gpu->multiply(lhs, rhs);
-    } catch (char const* e) {
-        fprintf(stderr, "%s\n", e);
-        throw e;
-    }
+    Matrix gpuMatrix = gpu->multiply(lhs, rhs);
     steady_clock::time_point gpuEnd = std::chrono::steady_clock::now();
 
     Operations* cpu = new CpuOperations();
 
     steady_clock::time_point cpuStart = steady_clock::now();
-//    Matrix cpuMatrix = cpu->multiply(lhs, rhs);
+    Matrix cpuMatrix = cpu->multiply(lhs, rhs);
     steady_clock::time_point cpuEnd = steady_clock::now();
 
     duration<double> cpuSpan = duration_cast<std::chrono::duration<double> >(cpuEnd - cpuStart);
     duration<double> gpuSpan = duration_cast<std::chrono::duration<double> >(gpuEnd - gpuStart);
 
-/*
     if (cpuMatrix != gpuMatrix)
     {
         printf("Matrix mismatch\n");
     }
-*/
 
     delete gpu;
     delete cpu;
@@ -55,20 +48,18 @@ int main()
 
     std::vector<Measurement> resultSet;
 
-    for (int i = 1; i < 41; i++)
+    for (int i = 1; i < 2; i++)
     {
-        int width = i * i;
-        int height = i * i;
-        Matrix lhs = Matrix::random(width, height, 10);
+        int width = 3 * i;
+        int height = 2 * i;
+        Matrix lhs = Matrix::random(width, height, 20);
         Matrix rhs = lhs.transpose();
 
         Measurement result = measureMultiply(lhs, rhs);
 
-        /*
         printf("%dx%d CPU: %.4f \n", width, height, result.cpu);
         printf("%dx%d GPU: %.4f \n", width, height, result.gpu);
-        */
-        fprintf(stderr, "%d;%d;%d;%.4f;%.4f;\n", i, width, height, result.cpu * 1000, result.gpu * 1000);
+        //fprintf(stderr, "%d;%d;%d;%.4f;%.4f;\n", i, width, height, result.cpu * 1000, result.gpu * 1000);
         //printf("%dx%d GPU: %.4f \n", width, height, result.gpu);
 
         resultSet.push_back(result);
